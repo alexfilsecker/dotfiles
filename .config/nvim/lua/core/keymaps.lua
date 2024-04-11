e1053bcacded8a0a555228ebaa69abba417c4ed1
Alex <alexfilsecker@gmail.com>
2024-04-11 12:27:33 -0400

    resize remaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -4,11 +4,24 @@ vim.g.mapleader = " "
 -- Acces Explorer
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
--- Navigate vim panes
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- Navigate vim panes (same as in tmux)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+-- Resize panes
+-- Could not make this to be as intuitive as in tmux
+-- In tmux, pane resizing works by moving the line between panes
+-- Here it makes a pane bigger or smaller so it is not an intuitive motion
+vim.keymap.set('n', '<C-M-h>', ':vertical resize +2<CR>')
+vim.keymap.set('n', '<C-M-j>', ':resize -2<CR>')
+vim.keymap.set('n', '<C-M-k>', ':resize +2<CR>')
+vim.keymap.set('n', '<C-M-l>', ':vertical resize -2<CR>')
+
+-- Split views
+vim.keymap.set('n', '<leader>|', vim.cmd.vsplit)
+vim.keymap.set('n', '<leader>-', vim.cmd.split)
 
 -- External clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', {})
@@ -21,6 +34,4 @@ vim.keymap.set('n', '<leader>hn', vim.cmd.nohlsearch)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
--- Split views
-vim.keymap.set('n', '<leader>|', vim.cmd.vsplit)
-vim.keymap.set('n', '<leader>-', vim.cmd.split)
+
