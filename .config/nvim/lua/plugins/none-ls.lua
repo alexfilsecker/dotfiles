3407e44eeea37f2dc98bf6f2b96ce9b21d48b537
Alex <alexfilsecker@gmail.com>
2024-04-18 21:16:13 -0400

    lsp super

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,28 @@
 -- This plugin is for the formatings
 
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-      },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"jay-babu/mason-null-ls.nvim",
+	},
+
+	config = function()
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "eslint_d",
+      }
     })
 
-    vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-  end,
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+	end,
 }
