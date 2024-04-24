a55e48cbc60cfd50e9da18d3ee6874d089bf7292
Alex <alexfilsecker@gmail.com>
2024-04-24 17:47:40 -0400

    Add comments to task_buttons

--- .config/awesome/binding/task_buttons.lua
+++ .config/awesome/binding/task_buttons.lua
@@ -2,6 +2,7 @@ local awful = require("awful")
 local gears = require("gears")
 
 return gears.table.join(
+  -- < LeftClick > Toogles minimzation and activates the client
   awful.button({}, 1, function(c)
     if c == client.focus then
       c.minimized = true
@@ -9,12 +10,18 @@ return gears.table.join(
       c:emit_signal("request::activate", "tasklist", { raise = true })
     end
   end),
+
+  -- < RightClick > Opens a menu with all clients
   awful.button({}, 3, function()
     awful.menu.client_list({ theme = { width = 250 } })
   end),
+
+  -- I do not have 4th click. But I'll leave this for the future
   awful.button({}, 4, function()
     awful.client.focus.byidx(1)
   end),
+
+  -- I do not have 4th click. But I'll leave this for the future
   awful.button({}, 5, function()
     awful.client.focus.byidx(-1)
   end)
