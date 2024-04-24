0b12abe8d1e02d27946922001ceb9e47f736f959
Alex <alexfilsecker@gmail.com>
2024-04-24 00:48:28 -0400

    use telescope to search in help

--- .config/nvim/lua/alex/plugins/telescope.lua
+++ .config/nvim/lua/alex/plugins/telescope.lua
@@ -37,6 +37,7 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Search across help" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
