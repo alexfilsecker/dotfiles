68dd2b3e89ed4a8c984341d62b96698530458261
Alex <alexfilsecker@gmail.com>
2024-04-24 16:32:36 -0400

    comments on tag_buttons.lua

--- .config/awesome/binding/tag_buttons.lua
+++ .config/awesome/binding/tag_buttons.lua
@@ -6,12 +6,12 @@ local modkey = user_variables.modkey
 
 return gears.table.join(
 
-  -- Right click on tab to view it
+  -- < LeftClick > Right click on tab to view it
   awful.button({}, 1, function(t)
     t:view_only()
   end),
 
-  --
+  -- < Sueper + LeftClick > Move client to tag and focus that tag
   awful.button({ modkey }, 1, function(t)
     if client.focus then
       client.focus:move_to_tag(t)
@@ -19,22 +19,22 @@ return gears.table.join(
     end
   end),
 
-  --
+  -- < RightClick > Toggle tag display
   awful.button({}, 3, awful.tag.viewtoggle),
 
-  --
+  -- < Super + RightClick > Toggle focused client on tag
   awful.button({ modkey }, 3, function(t)
     if client.focus then
       client.focus:toggle_tag(t)
     end
   end),
 
-  --
+  -- I do not have 4th click. But I'll leave this for the future
   awful.button({}, 4, function(t)
     awful.tag.viewnext(t.screen)
   end),
 
-  --
+  -- I do not have 4th click. But I'll leave this for the future
   awful.button({}, 5, function(t)
     awful.tag.viewprev(t.screen)
   end)
