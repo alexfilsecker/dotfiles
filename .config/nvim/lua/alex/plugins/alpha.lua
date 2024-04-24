f57a8f25de1ca483bc1cfad5279bf2f171db5fe7
Alex <alexfilsecker@gmail.com>
2024-04-24 00:47:47 -0400

    Better menu on alpha

--- .config/nvim/lua/alex/plugins/alpha.lua
+++ .config/nvim/lua/alex/plugins/alpha.lua
@@ -1,3 +1,5 @@
+-- This plugin gives me an awesome greeter
+
 return {
 	"goolord/alpha-nvim",
 	event = "VimEnter",
@@ -19,11 +21,12 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", " > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", " > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", " > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("Space wr", "󰁯  > Restore session for cwd", "<cmd>SessionRestore<CR>"),
+			dashboard.button("Space ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("Space ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("Space fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
