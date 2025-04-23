c49d25c511279d4eba15343efee79f8dc2be573f
Alex <alexfilsecker@gmail.com>
2025-04-22 20:10:22 -0400

    update on trouble plugin

--- .config/nvim/lua/alex/plugins/trouble.lua
+++ .config/nvim/lua/alex/plugins/trouble.lua
@@ -3,33 +3,38 @@
 
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
     {
       "<leader>xx",
-      "<cmd>TroubleToggle<CR>",
-      desc = "Open/close trouble list",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
     },
     {
-      "<leader>xw",
-      "<cmd>TroubleToggle workspace_diagnostics<CR>",
-      desc = "Open trouble workspace diagnostics",
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
     },
     {
-      "<leader>xd",
-      "<cmd>TroubleToggle document_diagnostics<CR>",
-      desc = "Open trouble document diagnostics",
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
     },
     {
-      "<leader>xq",
-      "<cmd>TroubleToggle quickfix<CR>",
-      desc = "Open trouble quickfix list",
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
     },
     {
-      "<leader>xl",
-      "<cmd>TroubleToggle loclist<CR>",
-      desc = "Open trouble location list",
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
     },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 }
